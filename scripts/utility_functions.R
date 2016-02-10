32d23c93e48332925912d8ff72e62b4eb4f521c2
Dan McGlinn <danmcglinn@gmail.com>
2016-02-10 00:03:04 -0500

    Add r2_adj function for direct ordinations.

--- scripts/utility_functions.R
+++ scripts/utility_functions.R
@@ -83,3 +83,79 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor=3, ...)
 pseudo_r2 = function(glm_mod) {
     1 -  glm_mod$deviance / glm_mod$null.deviance
 }
+
+r2_adj = function(Y, X, Z, method, nperm, dummy=0) {
+  ## Returns
+  ## a vector of R2, R2adj, and all R2adj replicates that result from permuations
+  ## Arguments
+  ## Y, X, Z: are species matrix, expl matrix, covar matrix
+  ## nperm: the number of permutations to perform, 
+  ##  if nperm not specified the analytical r2 and/or r2adj is returned 
+  ##  Note: for CCA only the permutation based r2 adj is unbiased 
+  ## method: specifies "cca" or "rda"
+  ## dummy: a number 0, 1 or 2 depending on how many collinear variables are in the
+  ##  explanatory matrix
+  ##  dummy is only necessary for the analytical R2adj calculation
+  Y = as.matrix(Y)
+  X = as.matrix(X)
+  if (missing(Z)) {
+    cca.emp = eval(parse(text= paste(method, '(Y, X)')))
+    r2 = cca.emp$CCA$tot.chi / cca.emp$tot.chi 
+    if (missing(nperm)) {
+      n = nrow(Y)
+      p = ncol(X) - dummy
+      out = c(r2, 1 - (((n - 1) / (n - p - 1)) * (1 - r2)))
+    }
+    else {
+      if (nperm <= 0)
+        stop('nperm argument must either be a positive integer or not specified')
+      rand.r2 = rep(NA, nperm)
+      i = 1
+      while (i <= nperm) {
+        Xrand = X[sample(nrow(X)), ]
+        cca.rand = try(eval(parse(text=paste(method, '(Y, Xrand)'))), TRUE)
+        if (class(cca.rand) == 'cca') {
+          rand.r2[i] = cca.rand$CCA$tot.chi
+          if (i %% 100 == 0)
+            print(i)
+          i = i + 1
+        }
+      }
+      out = c(r2, 
+              1 - (1 / (1 - mean(rand.r2 / cca.emp$tot.chi))) * (1 - r2),
+              1 - (1 / (1 - rand.r2 / cca.emp$tot.chi)) * (1 - r2))
+    }
+  }  
+  else{
+    Z = as.matrix(Z)
+    cca.emp = eval(parse(text=paste(method, '(Y, X, Z)')))
+    r2 = cca.emp$CCA$tot.chi / cca.emp$tot.chi
+    if (missing(nperm)) {
+      n = nrow(Y)
+      p = ncol(X) - dummy
+      out = c(r2, 1 - (((n - 1)/(n - p - 1)) * (1 - r2)))
+    }
+    else{
+      if (nperm <= 0)
+        stop('nperm argument must either be a positive integer or not specified')
+      rand.r2 = rep(NA, nperm)
+      i = 1
+      while (i <= nperm) {
+        rhold = sample(nrow(X))
+        Xrand = X[rhold, ]
+        Zrand = Z[rhold, ]
+        cca.rand = try(eval(parse(text=paste(method, '(Y, Xrand, Zrand)'))), TRUE)
+        if (class(cca.rand) == 'cca') {
+          rand.r2[i] = cca.rand$CCA$tot.chi
+          if (i %% 100 == 0)
+            print(i)
+          i = i + 1
+        }
+      }
+      out = c(r2,
+              1 - (1 / (1 - mean(rand.r2 / cca.emp$tot.chi))) * (1 - r2),
+              1 - (1 / (1 - rand.r2 / cca.emp$tot.chi)) * (1 - r2))
+    }  
+  }
+  return(out)
+}
