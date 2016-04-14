f63b4e694f34d74e6146a3efa5a13d79b52cea51
Dan McGlinn <danmcglinn@gmail.com>
2016-04-14 12:25:09 -0400

    Fix broken r2_adj_cca function and rename it r2_adj_ord.

--- scripts/utility_functions.R
+++ scripts/utility_functions.R
@@ -84,48 +84,48 @@ pseudo_r2 = function(glm_mod) {
     1 -  glm_mod$deviance / glm_mod$null.deviance
 }
 
-r2_adj_cca = function(cca_obj, nperm, analytical=FALSE) {
-    ## From Eq. 4 and 5 of Peres-Neto et al. 2006 - Ecology
-    ## Returns
-    ## a vector of R2, R2adj
-    ## Arguments
-    ## cca_obj: the output of rda or cca that was specified using a model formula
-    ## nperm: the number of permutations to perform, 
-    ##  if nperm not specified the analytical r2 and/or r2adj is returned 
-    ##  Note: for CCA only the permutation based r2 adj is unbiased 
-    ##  explanatory matrix
-    r2 = cca_obj$CCA$tot.chi / cca_obj$tot.chi 
-    n = nrow(cca_obj$CCA$Xbar)
+r2_adj_ord = function(ord_obj, nperm) {
+    # From Eq. 4 and 5 of Peres-Neto et al. 2006 - Ecology
+    # Returns
+    # a vector of R2, R2adj
+    # Arguments
+    # ord_obj: the output of rda or cca that was specified using a model formula
+    # nperm: the number of permutations to perform, 
+    #  if nperm not specified the analytical r2 and/or r2adj is returned 
+    # Note: for CCA only the permutation based r2 adj is unbiased 
+    if (!(ord_obj$method %in% c('rda', 'cca')))
+        stop('Ordination object must be the result of a cca or rda')
+    r2 = ord_obj$CCA$tot.chi / ord_obj$tot.chi 
+    n = nrow(ord_obj$CCA$Xbar)
     if (missing(nperm)) {
         # eq 4 Peres-Neto
-        p = cca_obj$CCA$rank
+        p = ord_obj$CCA$rank
         out = c(r2, 1 - (((n - 1) / (n - p - 1)) * (1 - r2)))
+        if (ord_obj$method == 'cca')
+            warning('The analytical formulation is only unbiased for rda')
     }
     else {
         if (nperm <= 0)
             stop('nperm argument must either be a positive integer or not specified')
-        if (!any(grepl('~', cca_obj$call)))
+        if (!any(grepl('~', ord_obj$call)))
             stop('The model object must be specified using a model formula rather than providing individual vectors / matrices')
-        rand.r2 = rep(NA, nperm)
-        Y_string = as.character(cca_obj$terms[[2]])
+        rand_r2 = rep(NA, nperm)
+        Y_string = as.character(ord_obj$terms[[2]])
         Y = eval(parse(text=Y_string))
+        Y_name = strsplit(as.character(ord_obj$call[2]), ' ~ ')[[1]][1]
+        ord_obj$call[2] = sub(Y_name, 'Y_rand', ord_obj$call[2])
         for (i in 1:nperm) {
-            Yrand = Y[sample(n), ]
-            #assign(Y_string, Yrand)
-            #cca.rand = eval(cca_obj$call)
-            cca_obj$call[2] = sub('comm', 'Yrand', cca_obj$call[2])
-            cca.rand = eval(parse(text=paste(cca_obj$call[1], '(',cca_obj$call[2], 
-                                             ', data=', cca_obj$call[3], ')', 
+            Y_rand = Y[sample(n), ]
+            cca_rand = eval(parse(text=paste(ord_obj$call[1], '(',ord_obj$call[2], 
+                                             ', data=', ord_obj$call[3], ')', 
                                              sep='')))
-            #cca.rand = update(cca_obj, Yrand ~ .)
-            rand.r2[i] = cca.rand$CCA$tot.chi / cca_obj$tot.chi
+            rand_r2[i] = cca.rand$CCA$tot.chi / ord_obj$tot.chi
             if (i %% 100 == 0)  print(i)
         }
         # Eq 5 Peres-Neto
         out = c(r2, 
-                1 - ((1 - r2) / (1 - mean(rand.r2))))
+                1 - ((1 - r2) / (1 - mean(rand_r2))))
     }
     names(out) = c('r2', 'r2adj')
     return(out)
 }
-
