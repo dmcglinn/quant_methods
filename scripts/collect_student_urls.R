aab1bdefce3419e1311bef70443dd4c03fde596f
Dan McGlinn <danmcglinn@gmail.com>
2023-01-10 16:20:33 +0000

    update collect student url script

--- scripts/collect_student_urls.R
+++ scripts/collect_student_urls.R
@@ -1,4 +1,21 @@
-year = 2019
+# clone repos from data frame
+
+stud <- read.csv('./data/Rclass_spring20.csv')
+stud$HW.url
+# drop last row
+stud <- stud[-nrow(stud), ]
+
+year = 2020
+student_path = paste0('./student_', year)
+
+for (i in seq_along(stud$HW.url)) {
+  #dir.create(paste0(student_path, '/', stud$Username[i]))
+  system(paste0('cd ', student_path, '; git clone ',
+                stud$HW.url[i], ' ', stud$Username[i]),
+         intern=TRUE)[1]
+}
+
+year = 2020
 student_path = paste0('./student_', year)
 students = dir(student_path)
 git_urls = list()
@@ -7,6 +24,7 @@ for(i in seq_along(students)) {
                          ' ; git remote -v'), intern=TRUE)[1]
 }
     
+## older code useful if you've already cloned all student repos
 
 git_urls = unlist(git_urls)
 git_urls = sub('origin\t', '', git_urls, fixed=T)
